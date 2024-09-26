ae02b47f1eceec4e8019136501bca0190eec57fe
dex0id <126304247+dex0id@users.noreply.github.com>
2024-09-26 12:02:59 -0400

    changing colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,11 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        'cl-purple': '#ae65b7',
+        'cl-yellow': '#b8cd42',
+        'cl-blue': '#47b8b4'
+      },
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
