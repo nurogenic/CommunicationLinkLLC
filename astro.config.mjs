9919f147227044922be89a3e9f0f21f064007ac8
dex0id <126304247+dex0id@users.noreply.github.com>
2025-08-27 11:00:05 -0400

    changing image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   site: 'https://communicationlinkllc.com/',
   integrations: [tailwind(), mdx(), sitemap()],
   adapter: cloudflare({
-    imageService: 'cloudflare',
+    imageService: 'compile',
     platformProxy: {
       enabled: true
     }
